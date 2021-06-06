93da504c69e117e1b8b53d9f4b45f9f89d389e71
Roman666shiyanov <60029536+Roman666shiyanov@users.noreply.github.com>
2021-06-07 01:35:31 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Kolj
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public double kW(double с)
+        {
+            double k = с * 100 /13;
+			return k;
+        }
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = SW(Convert.ToDouble(First.Text));
+                double result = a;
+
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Ошибка введенных данных!", "Ввести заново");
+            }
+        }
+    }
+
+}
+
+
